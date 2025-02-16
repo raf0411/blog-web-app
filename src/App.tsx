3de6a60b4abe319e9d97001284cec348fd51517c
Raffi <raffidarmawan0411@gmail.com>
2025-02-16 22:14:04 +0700

    set up MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
-import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Prompt, Arial, sans-serif",
+    h1: { fontSize: "2.5rem", fontWeight: 700 },
+    body1: { fontSize: "1rem" },
+  },
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#f50057", 
+    },
+    background: {
+      default: "#f4f4f4", 
+    },
+  },
+});
 
 function App() {
   return (
